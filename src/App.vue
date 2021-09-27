3d4080f8955ea306fceb311424d96a6519e9e521
Emi <e.wong@hotmail.co.uk>
2021-09-27 11:29:18 +0100

    create list component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item label="My ToDo Item"></to-do-item>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import ToDoItem from './components/ToDoItem.vue';
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    ToDoItem
+  }
 };
 </script>
 
