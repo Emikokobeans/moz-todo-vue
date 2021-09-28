db23cdd3b13902da09d1ed061ae0ef1ed66473d3
Emi <e.wong@hotmail.co.uk>
2021-09-28 12:11:55 +0100

    cancel edit method

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="stack-small">
+  <div class="stack-small" v-if="!isEditing">
     <div class="custom-checkbox">
       <input
         type="checkbox"
@@ -19,10 +19,23 @@
       </button>
     </div>
   </div>
+  <to-do-item-edit-form
+    v-else
+    :id="id"
+    :label="label"
+    @item-edited="itemEdited"
+    @edit-cancelled="editCancelled"
+  >
+  </to-do-item-edit-form>
 </template>
 
 <script>
+import ToDoItemEditForm from './ToDoItemEditForm';
+
 export default {
+  components: {
+    ToDoItemEditForm
+  },
   props: {
     label: { required: true, type: String },
     done: { default: false, type: Boolean },
@@ -40,6 +53,13 @@ export default {
     },
     toggleToItemEditForm() {
       this.isEditing = true;
+    },
+    itemEdited(newLabel) {
+      this.$emit('item-edited', newLabel);
+      this.isEditing = false;
+    },
+    editCancelled() {
+      this.isEditing = false;
     }
   }
 };
